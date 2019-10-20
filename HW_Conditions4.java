f90ab69cfc88d4c5aedf13aab535bd2cea38d35d
Ara Kin <ara_kin@inbox.ru>
2019-10-20 22:51:51 +0300

    fifth commit

--- HW_Conditions4.java
+++ HW_Conditions4.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class HW_Conditions4 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = s.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default: {
+                System.out.println("Value must be in range : 1-7");
+            }
+        }
+    }
+
 }
+
